3c81286903a3e5f58eb4fd4b59568d4b891983ad
gyoung410 <giyoung@bas.ac.uk>
2019-02-06 16:15:09 +0000

    compare runs 5 and 6

--- Analysis/halley_timeseries.py
+++ Analysis/halley_timeseries.py
@@ -1,16 +1,16 @@
 ##-----------------------------------------------------------------------------------------------------------------------
 ##-----------------------------------------------------------------------------------------------------------------------
 # The first line in a time-series output looks like this:
-# 	
+#
 # 	NZCM McMurdo               2  7 mcm   (-77.850, 166.710) ( 153, 207) (-77.768, 166.500)   81.8 meters
-# 	
+#
 # 	Those are name of the station, grid ID, time-series ID, station lat/lon, grid indices (nearest grid point to
 # 	the station location), grid lat/lon, elevation.
-# 	
+#
 # 	The variables from the time series output are:
-# 	
+#
 # 	id, ts_hour, id_tsloc, ix, iy, t, q, u, v, psfc, glw, gsw, hfx, lh, tsk, tslb(1), rainc, rainnc, clw
-# 	
+#
 # 	id:             grid ID
 # 	ts_hour:        forecast time in hours
 # 	id_tsloc:       time series ID
@@ -48,9 +48,15 @@ import pandas as pd
 ## Load in data
 ###################################
 
-## 
-file1_dir = '2_Nisg80_ThompsonDefault/'
-file2_dir = '3_Nisg80_ThompsonAeroClim/'
+## 2_Nisg80_ThompsonDefault/
+## 3_Nisg80_ThompsonAeroClim/
+## 4_Nisg80_Thompson_naCCN0408_naCCN1100/
+## 5_Archer_Default_AeroClim/
+## 6_Archer_NWFApl100_AeroClim/
+
+file_dir1 = '5_Archer_Default_AeroClim/'
+file_dir2 = '6_Archer_NWFApl100_AeroClim/'
+
 index = 'psfc'
 
 root_dir = '/data/mac/giyoung/MAC_WRFThompson/'
@@ -72,7 +78,7 @@ df2 = pd.DataFrame(file2)
 ###################################
 ## Quick check
 ###################################
- 
+
 df1.iloc[0,:] # prints first row of data
 df1.head()
 
