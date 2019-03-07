e40ae3a721490adcf8775025f5084942d74c1446
Gillian Young <eargy@see4-223.leeds.ac.uk>
2019-03-07 13:06:30 +0000

    update new run notes

--- Analysis/qncloud.py
+++ Analysis/qncloud.py
@@ -19,6 +19,13 @@ import cartopy.feature as cfe
 ## 4_Nisg80_Thompson_naCCN0408_naCCN1100/
 ## 5_Archer_Default_AeroClim
 ## 6_Archer_NWFApl100_AeroClim
+## 7_Archer_INITpl100/
+## 8_Archer_INITpl100_DRIVERpl100/
+## 9_Archer_DRIVER_NWFA1D_100e6/
+## 10_Archer_DRIVER_NWFA1D_100/
+## 11_Archer_DRIVER_NWFA1D_100e3/
+## 12_Archer_DRIVER_NWFA1D_x2/
+## 13_Archer_DRIVER_NWFA1D_x05/
 
 file_dir1 = '5_Archer_Default_AeroClim/'
 file_dir2 = '4_Nisg80_Thompson_naCCN0408_naCCN1100/'
