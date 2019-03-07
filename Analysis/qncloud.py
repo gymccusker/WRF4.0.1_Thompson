c5df9545d02daa45bd321ad2302c5dd59ba4f995
gyoung410 <g.young1@leeds.ac.uk>
2019-03-07 16:46:07 +0000

    update for use on Jasmin

--- Analysis/qncloud.py
+++ Analysis/qncloud.py
@@ -30,7 +30,7 @@ import cartopy.feature as cfe
 file_dir1 = '5_Archer_Default_AeroClim/'
 file_dir2 = '11_Archer_DRIVER_NWFA1D_100e3/'
 
-root_dir = '/data/mac/giyoung/MAC_WRFThompson/'
+root_dir = '/gws/nopw/j04/ncas_weather/gyoung/MAC/WRF_V4.0.1/RUNS/'
 
 time_index = 32
 
