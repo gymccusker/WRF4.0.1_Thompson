f9873c210f62a8f02a660fd9044bf2e7a1888ad8
gyoung410 <g.young1@leeds.ac.uk>
2019-03-13 16:53:21 +0000

    2m temp

--- Analysis/halley_timeseries.py
+++ Analysis/halley_timeseries.py
@@ -67,7 +67,7 @@ import pandas as pd
 file_dir1 = '3_Nisg80_ThompsonAeroClim/'
 file_dir2 = '10_Archer_DRIVER_NWFA1D_100/'
 
-index = 'gsw'
+index = 't'
 
 root_dir = '/gws/nopw/j04/ncas_weather/gyoung/MAC/WRF_V4.0.1/RUNS/'
 
