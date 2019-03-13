52e08cd1b8c974d6335dee1901b645ca4f6ccaab
gyoung410 <g.young1@leeds.ac.uk>
2019-03-13 16:55:24 +0000

    GSW, runs 3 and 11

--- Analysis/halley_timeseries.py
+++ Analysis/halley_timeseries.py
@@ -65,9 +65,9 @@ import pandas as pd
 ## 16_Archer_DRIVER_NWFA1D_100e3_K1/
 
 file_dir1 = '3_Nisg80_ThompsonAeroClim/'
-file_dir2 = '10_Archer_DRIVER_NWFA1D_100/'
+file_dir2 = '11_Archer_DRIVER_NWFA1D_100e3/'
 
-index = 't'
+index = 'gsw'
 
 root_dir = '/gws/nopw/j04/ncas_weather/gyoung/MAC/WRF_V4.0.1/RUNS/'
 
