ad08989069d66ed39d5e0eea74fc8b0216431b2b
gyoung410 <G.Young1@leeds.ac.uk>
2019-04-09 10:58:51 +0100

    only plot obs data from 27-Nov

--- Analysis/halley_timeseries.py
+++ Analysis/halley_timeseries.py
@@ -143,7 +143,7 @@ ax  = fig.add_axes([0.2,0.15,0.7,0.7])	# left, bottom, width, height
 
 plt.plot(df1.loc[np.size(df1.values[:,0])/float(2)-1:,'ts_hour']-24,df1.loc[np.size(df1.values[:,0])/float(2)-1:,index],label = 'Default')
 plt.plot(df2.loc[np.size(df2.values[:,0])/float(2)-1:,'ts_hour']-24,df2.loc[np.size(df2.values[:,0])/float(2)-1:,index],label = 'AeroClim')
-plt.plot(time_obs, gsw_obs, 'k')
+plt.plot(time_obs[0:2021], gsw_obs[0:2021], 'k', label = 'Obs')
 plt.xlabel('Time, h [27-Nov-2018]')
 plt.ylabel('Net SW flux at surface ($W/m^2$)')
 plt.xlim([0,24])
