49fcc05eb5e908ce8c9bc51fba8a2e3b2d972fd9
gyoung410 <G.Young1@leeds.ac.uk>
2019-04-09 09:28:21 +0100

    try returning data from defName

--- Analysis/plotDIAGS.py
+++ Analysis/plotDIAGS.py
@@ -64,6 +64,8 @@ def defName(data, var):
     if var == 'QNCLOUD': data.name = 'Cloud droplet number conc, cm-3'
     if var == 'QNWFA': data.name = 'water-friendly aerosol number con, cm-3'
 
+    return data
+
 def chooseData(nc1, nc2, var, time_index):
 
     ###################################
